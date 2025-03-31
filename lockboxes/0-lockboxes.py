443a3a2f80ef7e421f48b8e63e3827f826388d40
ArseneGiriteka <giritekaaarsene@gmail.com>
2025-03-31 12:09:33 +0200

    Lockboxes

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Lockboxes
+"""
+
+
+def canUnlockAll(boxes):
+    unlocked_box = []
+    for box in boxes:
+        if 0 <= box and box >= (len(boxes) - 1) and box not in unlocked_box:
+            unlocked_box += [box]
+        else:
+            return False
+    return True
