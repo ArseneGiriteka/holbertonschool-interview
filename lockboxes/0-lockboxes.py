57a42087afc944062528cbdd2e04dd1173823f7c
ArseneGiriteka <giritekaaarsene@gmail.com>
2025-03-31 12:13:01 +0200

    lockboxes

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -7,7 +7,8 @@ Lockboxes
 def canUnlockAll(boxes):
     unlocked_box = []
     for box in boxes:
-        if 0 <= box and box >= (len(boxes) - 1) and box not in unlocked_box:
+        if isinstance(box, int) and 0 <= box and box >= (len(boxes) - 1)\
+                and box not in unlocked_box:
             unlocked_box += [box]
         else:
             return False
