e4c165778e3eed8ead47071a07d6519fad0cde45
ArseneGiriteka <giritekaaarsene@gmail.com>
2025-04-04 09:17:44 +0200

    lockboxes

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-from collections import OrderedDict
 """
 Lockboxes:
 You have n number of locked boxes in front of you. Each box is numbered
 sequentially from 0 to n - 1 and each box may contain keys to the other
 boxes.
 """
+from collections import OrderedDict
 
 
 def canUnlockAll(boxes):
